61cd3fcf9dc4bd437ffb078831f1eb9931099ada
rivalnofirm <rivalnof2@gmail.com>
2021-04-04 04:25:13 +0700

    expenses module API

--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
-
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .serializers import ExpensesSerializer
+from .models import Expenses
+from rest_framework import permissions
+from .permissions import IsOwner
 # Create your views here.
+
+
+class ExpensesListAPIView(ListCreateAPIView):
+    serializer_class = ExpensesSerializer
+    queryset = Expenses.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        return serializer.save(owner = self.request.user)
+
+    def get_queryset(self):
+        return self.queryset.filter(owner=self.request.user)
+
+
+class ExpensesDetailAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = ExpensesSerializer
+    queryset = Expenses.objects.all()
+    permission_classes = (permissions.IsAuthenticated, IsOwner,)
+    lookup_field = "id"
+
+    def perform_create(self, serializer):
+        return serializer.save(owner = self.request.user)
+
+    def get_queryset(self):
+        return self.queryset.filter(owner=self.request.user)
