9533f4a63b09e75aeeec4e25ffa77db7b9f7894f
rivalnofirm <rivalnof2@gmail.com>
2021-04-05 14:30:19 +0700

    paginations

--- restapi/settings.py
+++ restapi/settings.py
@@ -96,6 +96,8 @@ DATABASES = {
 }
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
     'NON_FIELD_ERRORS_KEY': 'error',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
